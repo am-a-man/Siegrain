cdd11e6e9fd15dba14cf0511b587f409f41b0ebd
Aman Kumar <avy0219@gmail.com>
2021-06-15 02:53:43 +0530

    stage 1

--- website/script.js
+++ website/script.js
@@ -30,33 +30,33 @@ async function navInterface(arg){
     await getCommonItems();
     }
 
-    if(document.getElementById("showcase-pallete").getElementsByTagName('h1')[0].innerText.toLowerCase()==arg){
+    if(document.getElementById("showcase-title").innerText.toLowerCase()==arg){
         return;
     }
 
     switch (arg) {
         case "showcase":
             selectNav("showcase");
-            document.getElementById("showcase-pallete").getElementsByTagName('h1')[0].innerText="showcase";
+            document.getElementById("showcase-title").innerText="showcase";
             await openShowcase();
             break;
 
         case "info":
             selectNav("info");
-            document.getElementById("showcase-pallete").getElementsByTagName('h1')[0].innerText="info";
+            document.getElementById("showcase-title").innerText="info";
             await openInfo();
             break;
 
         case "updates":
             selectNav("updates");
-            document.getElementById("showcase-pallete").getElementsByTagName('h1')[0].innerText="updates";
+            document.getElementById("showcase-title").innerText="updates";
             await openUpdates();
             break;
     
         default:
             console.log(arg);
             selectNav("showcase");
-            document.getElementById("showcase-pallete").getElementsByTagName('h1')[0].innerText="showcase";
+            document.getElementById("showcase-title").innerText="showcase";
             openShowcase();
             break;
     }
@@ -103,12 +103,22 @@ async function getCommonItems()
     </div>
 
  
-    <div id="showcase-pallete">
-        <h1 style="text-transform:capitalize"></h1>
-        <ul id="showcase-items"></ul>
+    <h1 style="text-transform:capitalize" id="showcase-title"></h1>
+    <div id="showcase-pallete" class="carousel slide" data-ride="carousel">
+ 
+        
+        <ul id="showcase-items" class="carousel-inner"></ul>
+
+
+        <a class="carousel-control-prev" href="#showcase-pallete" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#showcase-pallete"             data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
     </div>
 
-
+  
 
     <div id="footer">
         <div id="left-footer"><p>
@@ -139,13 +149,16 @@ async function getCommonItems()
             <a href="https://www.instagram.com/am__a_man_/"><img src="https://cdn.jsdelivr.net/npm/simple-icons@5.0.0/icons/instagram.svg"> </a>
             
         </div>
-        <button type="button" onclick="moveContent(-1)"><img src=""> </button>
-        <button type="button" onclick="moveContent(+1)"><img src=""> </button>
+ 
     </div>
 
     `;
     
 }
+
+
+
+
 
 async function openInfo(){
     var showcaseItems = document.getElementById('showcase-items');
@@ -158,25 +171,41 @@ async function openInfo(){
 
     var UL = document.createElement('UL');
 
-
+    var UL2 = document.createElement('ul');
+    UL2.setAttribute("class","carousel-indicators");
     for(var i = 0; i < keys.length; i++)
     {
+        var LI2 = document.createElement('li')
+        LI2.setAttribute("data-target","#showcase-pallete");
+        LI2.setAttribute("data-slide-to",`${i}`);
+        if(i==0)
+            LI2.setAttribute("class","active");
+    
+        UL2.appendChild(LI2);
         var A = document.createElement('A');
-        
+        var DIV = document.createElement("div");
+        DIV.setAttribute("class" , "carousel-caption");
         var H1 = document.createElement('H1');
         var P = document.createElement('P')
+        DIV.appendChild(H1);
+        DIV.appendChild(P);
+
         H1.innerText = `${data['info'][keys[i]]['title']}:`;
         P.innerText = data['info'][keys[i]]['detail'];
 
-        A.appendChild(H1);
-        A.appendChild(P);
+        A.appendChild(DIV);
 
         A.setAttribute('href', data['info'][keys[i]]['href']);
     
         var LI = document.createElement('LI');
         LI.appendChild(A);
+        if(i==0)
+            LI.setAttribute("class", "carousel-item active")
+        else
+            LI.setAttribute("class", "carousel-item")
         UL.appendChild(LI);
     }
+    document.getElementById("showcase-pallete").appendChild(UL2);
 
     var listItems=UL.innerHTML;
     showcaseItems.innerHTML = listItems;
@@ -192,26 +221,41 @@ async function openUpdates(){
     var keys = Object.keys(data['updates']);
  
     var UL = document.createElement('ul');
-    
+    var UL2 = document.createElement('ul');
+    UL2.setAttribute("class","carousel-indicators");
     for(var i = 0; i < keys.length; i++)
     {
+        var LI2 = document.createElement('li')
+        LI2.setAttribute("data-target","#showcase-pallete");
+        LI2.setAttribute("data-slide-to",`${i}`);
+        if(i==0)
+            LI2.setAttribute("class","active");
+        UL2.appendChild(LI2);
         var A = document.createElement('A');
         
         var H1 = document.createElement('H1');
         var P = document.createElement('P')
+        var DIV = document.createElement("div");
+        DIV.setAttribute("class" , "carousel-caption");
         H1.innerText = `${data['updates'][keys[i]]['title']}:`;
         P.innerText = data['updates'][keys[i]]['detail'];
 
-        A.appendChild(H1);
-        A.appendChild(P);
-
+        DIV.appendChild(H1);
+        DIV.appendChild(P);
+        A.appendChild(DIV);
         A.setAttribute('href', data['updates'][keys[i]]['href']);
     
         var LI = document.createElement('LI');
         LI.appendChild(A);
+        if(i==0)
+            LI.setAttribute("class", "carousel-item active")
+        else
+            LI.setAttribute("class", "carousel-item")
+        
         UL.appendChild(LI);
+        
     }
-
+    document.getElementById("showcase-pallete").appendChild(UL2);
     var listItems=UL.innerHTML;
     showcaseItems.innerHTML=listItems;
 }
@@ -231,30 +275,47 @@ async function openShowcase()
     
     
     var UL = document.createElement('UL');
-  
+    var UL2 = document.createElement('ul');
+    UL2.setAttribute("class","carousel-indicators");
  
     for(var i = 0; i < keys.length; i++)
     {
+        var LI2 = document.createElement('li')
+        LI2.setAttribute("data-target","#showcase-pallete");
+        LI2.setAttribute("data-slide-to",`${i}`);
+        if(i==0)
+            LI2.setAttribute("class","active");
+        UL2.appendChild(LI2);
         var A = document.createElement('A');
+        var DIV = document.createElement("div");
+        DIV.setAttribute("class" , "carousel-caption");
         var IMG = document.createElement("IMG");
         var H1 = document.createElement('H1');
         var P = document.createElement('P')
+        
+        DIV.appendChild(H1);
+        DIV.appendChild(P);
+        
         H1.innerText = `${data['projects'][keys[i]]['title']}:`;
         P.innerText = data['projects'][keys[i]]['stack'];
-        console.log(data);
+
         IMG.setAttribute("src", `${data['projects'][keys[i]]["logo"]}`)
         IMG.setAttribute("alt",  "Project-logo");
         
         A.appendChild(IMG);
-        A.appendChild(H1);
-        A.appendChild(P);
+        A.appendChild(DIV); 
         
         A.setAttribute('href', data['projects'][keys[i]]['href']);
-
+        
         var LI = document.createElement('LI');
         LI.appendChild(A);
+        if(i==0)
+            LI.setAttribute("class", "carousel-item active")
+        else
+            LI.setAttribute("class", "carousel-item")
         UL.appendChild(LI);
     };
+    document.getElementById("showcase-pallete").appendChild(UL2);
 
     var listItems = UL.innerHTML;
     showcaseItems.innerHTML = listItems;
